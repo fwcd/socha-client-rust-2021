afb30f24092a2edbdf24adf501ab5ac3ef422dbc
fwcd <fwcdmail@gmail.com>
2022-01-29 20:45:48 +0100

    Add .editorconfig and update indentation

--- src/logic.rs
+++ src/logic.rs
@@ -9,16 +9,16 @@ use crate::{client::SCClientDelegate, game::{GameState, Team, Move}};
 pub struct OwnGameLogic;
 
 impl SCClientDelegate for OwnGameLogic {
-	fn request_move(&mut self, state: &GameState, _my_team: Team) -> Move {
-		// Implement custom game logic here!
-		let mut random = rand::thread_rng();
-		let moves: Vec<_> = state.possible_moves().collect();
-		let game_move = moves.choose(&mut random).cloned().expect("No move found");
-		info!("Chose {:?} from {} moves", game_move, moves.len());
-		game_move
-	}
-	
-	fn on_update_state(&mut self, state: &GameState) {
-		debug!("New board:\n{:?}", state.board);
-	}
+    fn request_move(&mut self, state: &GameState, _my_team: Team) -> Move {
+        // Implement custom game logic here!
+        let mut random = rand::thread_rng();
+        let moves: Vec<_> = state.possible_moves().collect();
+        let game_move = moves.choose(&mut random).cloned().expect("No move found");
+        info!("Chose {:?} from {} moves", game_move, moves.len());
+        game_move
+    }
+    
+    fn on_update_state(&mut self, state: &GameState) {
+        debug!("New board:\n{:?}", state.board);
+    }
 }
